90fd988587c33f4a4becd3e79cdc9bf022098bed
priyankabai <71543717+priyankabai@users.noreply.github.com>
2021-06-11 17:01:45 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,8 @@
+public class input{
+	 static String mailid = "shv2testing18@gmail.com";
+     static String firstName = "Priyanka";
+     static String lastName = "Nandeti";
+     static String contact = "9985641177";
+     static String company = "Cognicepts";
+     static String password = "Sona@1234";
+}
